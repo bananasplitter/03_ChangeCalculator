8ad4a53a0670f358a558b2bd46eaa551042addbc
bananasplitter <tiana_pehle@yahoo.com>
2016-01-25 14:48:31 -0800

    change calculator

---
+++ ChangeCalculator/ChangeCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asking for total costs of items
+            //calculate change
+            //determine w
+            decimal Dollar = 1.00;
+            decimal Quarter = .25;
+            decimal Dime = .10;
+            decimal Nickel = .05;
+            decimal Penny = .01;
+
+
+            Console.WriteLine("How much does items cost?");
+
+             
+            string costs = Console.ReadLine();
+            
+
+        }
+    }
+}
